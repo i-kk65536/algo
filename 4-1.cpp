86efb7739c672a61faaa0344dc70275b7f8528d0
i-kk65536 <130728870+i-kk65536@users.noreply.github.com>
2024-05-10 15:25:11 +0900

    4-1_v1.0.0

--- 4-1.cpp
+++ 4-1.cpp
@@ -49,16 +49,38 @@ public:
         }
     }
     void insert_x(int x) {
-
+		Elements* insert_prev = &dummy;
+		Elements* insert = new Elements();
+		insert->data = x;
+		insert->next = insert_prev->next;
+		insert->prev = insert_prev;
+		(insert_prev->next)->prev = insert;
+		insert_prev->next = insert;
     }
     void delete_x(int x) {
-
+		Elements* deleted = &dummmy;
+		bool find_flag = false;
+		while (deleted->next != &dummmy)
+		{
+			if (x == deleted->data)
+			{
+				find_flag = true;
+				break;
+			}
+			deleted = deleted->next;
+		}
+		if (!find_flag)
+			return
+		Elements* deleted_prev = deleted->prev;
+		deleted_prev->next = deleted->next;
+		(deleted->next)->prev = deleted_prev;
+		delete deleted
     }
     void deleteFirst() {
-
+		delete_x((dummy.next)->data)
     }
     void deleteLast() {
-
+		delete_x(dummy.data)
     }
 }
 
