d45d35668363a8a36dd0f9608f314e334346567b
i-kk65536 <130728870+i-kk65536@users.noreply.github.com>
2024-10-02 13:09:32 +0900

    17-2_v1.0.0

---
+++ 17-2_ALDS1_14_B.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class StrExpand
+{
+private:
+    string terget;
+
+public:
+    StrExpand(string t) : terget(t) {}
+    vector<int> find(string query)
+    {
+        vector<int> match_index;
+        if (query.length() > terget.length())
+            return match_index;
+            
+        for (int i = 0; i < terget.length() - (query.length() - 1); i++)
+        {
+            int pos_t = 0;
+            int pos_q = 0;
+            while (terget[i + pos_t] == query[pos_q])
+            {
+                if (query.length() - 1 == pos_q)
+                {
+                    match_index.emplace_back(i);
+                    break;
+                }
+                pos_t++;
+                pos_q++;
+            }
+        }
+        return match_index;
+    }
+};
+
+int main()
+{
+    string t, q;
+    cin >> t >> q;
+
+    StrExpand terget(t);
+    vector<int> match_index = terget.find(q);
+
+    for (const auto& e : match_index)
+    {
+        cout << e << endl;
+    }
+}
