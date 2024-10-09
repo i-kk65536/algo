4e919adc1f701f14537f860b50c60a7d23cfa418
i-kk65536 <lockegh512@gmail.com>
2024-10-09 13:42:26 +0900

    v1.0.0

--- 18-1_ALDS1_14_B.cpp
+++ 18-1_ALDS1_14_B.cpp
@@ -16,34 +16,64 @@ public:
         if (query.length() > text.length())
             return match_index;
         
-        vector<int> table = createPartialMatchTable()
-        int i, pos_t, pos_q = 0;
-        while (i < text.length() - (query.length() - 1))
+        vector<int> table = createPartialMatchTable(query);
+        int pos_t = 0;
+        int pos_q = 0;
+        while (pos_t < text.length())
         {
-            if (i != 0)
+            if (query[pos_q] == text[pos_t])
             {
-                pos_t = i;
-                pos_q = table[pos_q];
-            }
-            while (text[pos_t] == query[pos_q])
-            {
-                if (pos_q == query.length() - 1)
-                {
-                    match_index.emplace_back(i);
-                    break;
-                }
                 pos_t++;
                 pos_q++;
             }
-
-            pos_t = pos_t + pos_q - table[pos_q];
-            pos_q = (pos_q > 0 ? table[pos_q] : pos_q);
+            if (pos_q == query.length() - 1)
+            {
+                match_index.emplace_back(pos_t - pos_q);
+                pos_q = table[pos_q - 1];
+            }
+            else if (pos_t < text.length() && query[pos_q] != text[pos_t])
+            {
+                if (pos_q != 0)
+                {
+                    pos_q = table[pos_q - 1];
+                }
+                else
+                {
+                    pos_t++;
+                }
+            }
         }
         return match_index;
     }
-    vector<int> createPartialMatchTable()
+    vector<int> createPartialMatchTable(const string& query)
     {
+        vector<int> table(query.length());
+        int prefix_length = 0;
+        table[0] = 0;
+        int i = 1;
 
+        while (i < query.length())
+        {
+            if (query[i] == query[prefix_length])
+            {
+                prefix_length++;
+                table[i] = prefix_length;
+                i++;
+            }
+            else
+            {
+                if (prefix_length != 0)
+                {
+                    prefix_length = table[prefix_length - 1];
+                }
+                else
+                {
+                    table[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return table;
     }
 };
 
