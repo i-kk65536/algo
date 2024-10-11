b42ec2d8f227433503155dddee0efa02b17e035b
i-kk65536 <locke512@gmail.com>
2024-10-12 00:51:19 +0900

    IO高速化

--- 18-1_ALDS1_14_B.cpp
+++ 18-1_ALDS1_14_B.cpp
@@ -1,93 +1,98 @@
+#pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
 #include <iostream>
+#include <cctype>
+#include <cstdio>
+#include <utility>
 #include <string>
 #include <vector>
 using namespace std;
 
-class StrKMP
-{
-private:
-    string text;
-    vector<int> normalFind(const string& query)
-    {
-		for (int i = 0; i <= text.size() - query.size(); i++)
-        {
-			if (query == text.substr(i, query.size()))
-            {
-				cout << i << endl;
-			}
-        }
-        return vector<int>();
-    }
-    vector<int> KMPFind(const string& query)
-    {
-        vector<int> match_index;
-        vector<int> table = createPartialMatchTable(query);
-		int top_pos = 0;
-		int top_diff = 0;
-    	while (top_pos + top_diff < text.length())
-		{
-        	if (text[top_pos + top_diff] == query[top_diff])
-			{
-        		top_diff++;
-        		if (top_diff == query.length())
-        		{
-         	       match_index.emplace_back(top_pos);
-                   top_pos++;
-                   top_diff = 0;
-        		}
-			}
-        	else
-			{
-       	    	top_pos = top_pos + top_diff - table[top_diff];
-      	 		if (top_diff > 0)
-   	  	           top_diff = table[top_diff];
-			}
-		}
-        return match_index;
-    }
-    vector<int> createPartialMatchTable(const string& query)
-    {
-        vector<int> table(query.length());
-        table[0] = -1;
-        int j = -1;
-        for (int i = 0; i < query.length() - 1; i++)
-        {
-            while (j >= 0 && query[i] != query[j])
-            {
-                j = table[j];
-            }
-            table[i + 1] = j + 1;
-            j++;
-        }
-        return table;
-    }
-
+class IO {
 public:
-    StrKMP(const string& t) : text(t) {}
-    vector<int> find(const string& query)
+    static inline void read(string &s)
     {
-        vector<int> match_index;
-        if (query.length() > text.length())
-            return match_index;
-        
-        if (query.length() == 1)
-            match_index = normalFind(query);
-        else
-            match_index = KMPFind(query);
-        return match_index;
+        for (char c; !isspace(c = getchar_unlocked()); s += c);
+    }
+    static inline void write(int n)
+    {
+        static char buf[20];
+        char *p = buf;
+        while (n)
+        {
+            *p++ = n % 10 + '0';
+            n /= 10;
+        }
+        while (p != buf)
+        {
+            putchar_unlocked(*--p);
+        }
+    }
+    static inline void write(char c)
+    {
+        putchar_unlocked(c);
     }
 };
 
+void createPartialMatchTable(const string& query, vector<int>& table)
+{
+    int query_length = query.length();
+    table[0] = -1;
+    int j = -1;
+    for (int i = 0; i < query_length - 1; i++)
+    {
+        while (j >= 0 && query[i] != query[j])
+        {
+            j = table[j];
+        }
+        table[i + 1] = j + 1;
+        j++;
+    }
+}
+
+void KMPfind(const string& text, const string& query)
+{
+    int text_length = text.length();
+    int query_length = query.length();
+    if (query_length > text_length)
+        return;
+    
+    vector<int> table(query_length);
+    createPartialMatchTable(query, table);
+    int top_pos = 0;
+    int top_diff = 0;
+    while (top_pos + top_diff < text_length)
+    {
+        if (text[top_pos + top_diff] == query[top_diff])
+        {
+            top_diff++;
+            if (top_diff == query_length)
+            {
+                IO::write(top_pos);
+                IO::write('\n');
+                top_pos++;
+                top_diff = 0;
+            }
+        }
+        else
+        {
+            top_pos = top_pos + top_diff - table[top_diff];
+            if (top_diff > 0)
+                top_diff = table[top_diff];
+        }
+    }
+}
+
 int main()
 {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
     string text, query;
-    cin >> text >> query;
+    IO::read(text);
+    IO::read(query);
 
-    StrKMP terget(text);
-    vector<int> match_index = terget.find(query);
-
-    for (const auto& e : match_index)
-    {
-        cout << e << endl;
-    }
+    KMPfind(text, query);
 }
