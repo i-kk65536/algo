f72c3f5082b43a1d46c4534bd0198b8e6d41f1d8
i-kk65536 <locke512@gmail.com>
2024-10-13 01:24:16 +0900

    v3.0.0

--- 18-1_ALDS1_14_B.cpp
+++ 18-1_ALDS1_14_B.cpp
@@ -3,58 +3,67 @@
 #include <vector>
 using namespace std;
 
-void createPartialMatchTable(const string& query, vector<int>& table)
+class StrMP
 {
-    int query_length = query.length();
-    table[0] = -1;
-    int j = -1;
-    for (int i = 1; i < query_length; i++)
-    {
-        while (j >= 0 && query[i] != query[j + 1])
-        {
-            j = table[j];
-        }
-        if (query[i] == query[j + 1])
-        {
-            j++;
-        }
-        table[i] = j;
-    }
-}
+private:
+    string text;
 
-void MPfind(const string& text, const string& query)
-{
-    int text_length = text.length();
-    int query_length = query.length();
-    if (query_length > text_length)
-        return;
-    
-    vector<int> table(query_length);
-    createPartialMatchTable(query, table);
-    int text_pos = 0;
-    int top_diff = -1;
-    for (; text_pos < text_length; text_pos++)
+    void createPartialMatchTable(const string& query, vector<int>& table)
     {
-        while (top_diff >= 0 && text[text_pos] != query[top_diff + 1])
+        int query_length = query.length();
+        table[0] = -1;
+        int j = -1;
+        for (int i = 1; i < query_length; i++)
         {
-            top_diff = table[top_diff];
-        }
-        if (text[text_pos] == query[top_diff + 1])
-        {
-            top_diff++;
-        }
-        if (top_diff == query_length - 1)
-        {
-            cout << text_pos - (query_length - 1) << '\n';
-            top_diff = table[top_diff];
+            while (j >= 0 && query[i] != query[j + 1])
+            {
+                j = table[j];
+            }
+            if (query[i] == query[j + 1])
+            {
+                j++;
+            }
+            table[i] = j;
         }
     }
-}
+public:
+    StrMP(const string& text) : text(text) {}
+
+    void find(const string& query)
+    {
+        int text_length = text.length();
+        int query_length = query.length();
+        if (query_length > text_length)
+            return;
+        
+        vector<int> table(query_length);
+        createPartialMatchTable(query, table);
+        int text_pos = 0;
+        int top_diff = -1;
+        for (; text_pos < text_length; text_pos++)
+        {
+            while (top_diff >= 0 && text[text_pos] != query[top_diff + 1])
+            {
+                top_diff = table[top_diff];
+            }
+            if (text[text_pos] == query[top_diff + 1])
+            {
+                top_diff++;
+            }
+            if (top_diff == query_length - 1)
+            {
+                cout << text_pos - (query_length - 1) << '\n';
+                top_diff = table[top_diff];
+            }
+        }
+    }
+};
 
 int main()
 {
     string text, query;
     cin >> text >> query;
 
-    MPfind(text, query);
+    StrMP terget(text);
+    terget.find(query);
 }
