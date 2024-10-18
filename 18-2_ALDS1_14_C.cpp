2934496a70ee6f5f8ef99e6993412ca34beef3d1
i-kk65536 <locke512@gmail.com>
2024-10-18 20:09:40 +0900

    TLE

--- 18-2_ALDS1_14_C.cpp
+++ 18-2_ALDS1_14_C.cpp
@@ -6,36 +6,36 @@ using namespace std;
 int main() {
     int H, W;
     cin >> H >> W;
-    vector<string> field(H);
- 
-    for (int i = 0; i < H; ++i) {
-        cin >> field[i];
+    vector<string> target(H);
+    for (int i = 0; i < H; ++i)
+    {
+        cin >> target[i];
     }
- 
     int R, C;
     cin >> R >> C;
-    vector<string> pattern(R);
- 
-    for (int i = 0; i < R; ++i) {
-        cin >> pattern[i];
+    vector<string> query(R);
+    for (int i = 0; i < R; ++i)
+    {
+        cin >> query[i];
     }
- 
-    for (int i = 0; i <= H - R; ++i) {
-        for (int j = 0; j <= W - C; ++j) {
+
+    for (int i = 0; i <= H - R; ++i)
+    {
+        for (int j = 0; j <= W - C; ++j)
+        {
             bool match = true;
- 
-            for (int k = 0; k < R; ++k) {
-                if (field[i + k].compare(j, C, pattern[k]) != 0) {
+            for (int k = 0; k < R; ++k)
+            {
+                if (target[i + k].compare(j, C, query[k]) != 0)
+                {
                     match = false;
                     break;
                 }
             }
- 
-            if (match) {
+            if (match)
+            {
                 cout << i << " " << j << endl;
             }
         }
     }
- 
-    return 0;
 }
