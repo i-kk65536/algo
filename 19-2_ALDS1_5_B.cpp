f2a6455bbc65fd7f8d02887a8ad8fb936c417952
i-kk65536 <lockegh512@gmail.com>
2024-10-30 14:35:11 +0900

    ファイル名変更

--- 19-2_ALDS1_5_B.cpp
+++ 19-2_ALDS1_5_B.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 using namespace std;
 
-int margeSort(vector<int> list, size_t left, size_t right);
-int marge(vector<int> list, size_t left, size_t mid, size_t right);
+int mergeSort(vector<int>& list, size_t left, size_t right_next);
+int merge(vector<int>& list, size_t left, size_t mid, size_t right_next);
 
 int main()
 {
-
+    size_t size;
+    cin >> size;
+    vector<int> list(size);
+    for (int i = 0; i < size; i++)
+    {
+        cin >> list[i];
+    }
+    
+    int comparison_times = mergeSort(list, 0, size);
+    
+    for (int i = 0; i < size; i++)
+    {
+        cout << list[i];
+        if (i != size - 1)
+            cout << " ";
+        else
+            cout << endl;
+    }
+    cout << comparison_times << endl;
 }
 
-int marge(vector<int> list, size_t left, size_t mid, size_t right)
+int merge(vector<int>& list, size_t left, size_t mid, size_t right_next)
 {
     size_t end_L = mid - left;
-    size_t end_R = right - mid;
+    size_t end_R = right_next - mid;
     vector<int> list_L(end_L + 1);
     vector<int> list_R(end_R + 1);
     for (int i = 0; i < end_L; i++)
@@ -24,17 +43,40 @@ int marge(vector<int> list, size_t left, size_t mid, size_t right)
     {
         list_R[i] = list[mid + i];
     }
+    list_L[end_L] = INT_MAX;
+    list_R[end_R] = INT_MAX;
+
+    size_t index_L = 0;
+    size_t index_R = 0;
+    int comparison_times = 0;
+    for (int i = left; i < right_next; i++)
+    {
+        if (list_L[index_L] <= list_R[index_R])
+        {
+            list[i] = list_L[index_L];
+            index_L++;
+        }
+        else
+        {
+            list[i] = list_R[index_R];
+            index_R++;
+        }
+        comparison_times++;
+    }
+    return comparison_times;
 }
 
-int margeSort(vector<int> list, size_t left, size_t right)
+int mergeSort(vector<int>& list, size_t left, size_t right_next)
 {
     int comparison_times = 0;
-    if (!((left + 1) < right))
+    if (!(left + 1 < right_next))
+    {
         return comparison_times;
+    }
     
-    int mid = (left + right) / 2;
-    comparison_times += margeSort(list, left, mid);
-    comparison_times += margeSort(list, mid, right);
-    comparison_times += marge(list, left, mid, right);
+    size_t mid = (left + right_next) / 2;
+    comparison_times += mergeSort(list, left, mid);
+    comparison_times += mergeSort(list, mid, right_next);
+    comparison_times += merge(list, left, mid, right_next);
     return comparison_times;
 }
